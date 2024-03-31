c8eb6026c12eb6bfbd74e0fce2027186a1833133
bmla.nieling@student.han.nl <rick.niel@hotmail.com>
2024-03-31 12:42:08 +0200

    init commit

---
+++ src/includes/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+import firebaseConfig from '../../firebaseConfig';
+
+// Initialize Firebase - Create your own config file.
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+const usersCollection = db.collection('users');
+const songsCollection = db.collection('songs');
+
+export { auth, db, usersCollection, storage, songsCollection }
---
+++ src/router/index.js
@@ -0,0 +1,53 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/HomeView.vue';
+import About from '@/views/AboutView.vue';
+import Manage from '@/views/ManageView.vue';
+import NotFound from '@/views/NotFound.vue';
+import useUserStore from '@/stores/user';
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: About
+  },
+  {
+    name: 'manage',
+    // alias: ['/manage', '/test'],
+    path: '/manage-music',
+    component: Manage,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' }
+  },
+  {
+    path: '/:catchAll(.*)*',
+    component: NotFound
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: routes,
+  linkExactActiveClass: 'text-yellow-500'
+})
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
+    next();
+    return;
+  }
+
+  useUserStore().userLoggedIn ? next() : next({ name: 'home' });
+})
+
+export default router
