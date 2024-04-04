3c24a132fedaf429cbb5e64245ede92054311e68
Rick Nieling <b.nieling@vgz.nl>
2024-04-04 20:48:03 +0200

    directives

--- src/main.js
+++ src/main.js
@@ -4,22 +4,23 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import Icon from './directives/icon'
 import App from './App.vue'
 import router from './router'
 import VeeValidatePlugin from './includes/validation'
 import { auth } from './includes/firebase'
 
-let app;
+let app
 
 auth.onAuthStateChanged(() => {
-    if (!app) {
-        app = createApp(App)
+  if (!app) {
+    app = createApp(App)
 
-        app.use(createPinia())
-        app.use(router)
-        app.use(VeeValidatePlugin)
+    app.use(createPinia())
+    app.use(router)
+    app.use(VeeValidatePlugin)
+    app.directive('icon', Icon)
 
-        app.mount('#app')
-    }
-});
-
+    app.mount('#app')
+  }
+})
